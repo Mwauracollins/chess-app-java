62a7bec7455d2c86861aa3adf6cc25eeab6555fb
Mwauracollins <collinsmwaura8844@gmail.com>
2024-01-19 13:50:02 +0300

    Added the abstract Move class where different Moves inherit from

---
+++ com/engine/game/AttackMove.java
@@ -0,0 +1,14 @@
+package com.engine.game;
+
+import com.engine.board.Board;
+import com.engine.piece.Knight;
+import com.engine.piece.Piece;
+
+/**
+ * Created by user on 01 19 2024
+ **/
+public class AttackMove extends Move{
+    public AttackMove(Board board, Piece movedPiece, int row, int column, Piece attackedPiece) {
+        super();
+    }
+}
