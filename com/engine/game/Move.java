ba2ba2472f51f00c59c78fb620fc8b2cfea4e4ac
Mwauracollins <collinsmwaura8844@gmail.com>
2024-01-19 13:50:02 +0300

    Added the abstract Move class where different Moves inherit from

---
+++ com/engine/game/Move.java
@@ -0,0 +1,7 @@
+package com.engine.game;
+
+/**
+ * Created by user on 01 19 2024
+ **/
+public class Move {
+}
---
+++ com/engine/game/NormalMove.java
@@ -0,0 +1,14 @@
+package com.engine.game;
+
+import com.engine.board.Board;
+import com.engine.piece.Knight;
+import com.engine.piece.Piece;
+
+/**
+ * Created by user on 01 19 2024
+ **/
+public class NormalMove extends Move{
+    public NormalMove(Board board, Piece movedPiece, int row, int column) {
+        super();
+    }
+}
