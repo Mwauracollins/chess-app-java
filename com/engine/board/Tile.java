3011e5d84344857d957f64b162dee4d9377c4a05
Mwauracollins <collinsmwaura8844@gmail.com>
2024-01-19 11:21:34 +0300

    Added the tile class

---
+++ com/engine/board/Tile.java
@@ -0,0 +1,22 @@
+package com.engine.board;
+
+import com.engine.piece.Piece;
+
+/**
+ * Created by user on 01 19 2024
+ **/
+public abstract class Tile {
+    private int row;
+    private int column;
+
+    protected Tile(final int row, final int column){
+        this.row = row;
+        this.column = column;
+    }
+    public abstract Piece getPiece();
+    public abstract boolean isTileOccupied();
+    public int getTileId(){
+        return (row * 8) + column;
+    }
+
+}
