b3d93017937e6ae3d30280de549d004ef3fdc84f
Mwauracollins <collinsmwaura8844@gmail.com>
2024-01-19 11:38:52 +0300

    Added the EmptyTile and OccupiedTile classes

---
+++ com/engine/board/OccupiedTile.java
@@ -0,0 +1,24 @@
+package com.engine.board;
+
+import com.engine.piece.Piece;
+
+/**
+ * Created by user on 01 19 2024
+ **/
+public class OccupiedTile extends Tile{
+    private Piece pieceOnTile;
+    protected OccupiedTile(int row, int column, Piece pieceOnTile) {
+        super(row, column);
+        this.pieceOnTile = pieceOnTile;
+    }
+
+    @Override
+    public Piece getPiece() {
+        return this.pieceOnTile;
+    }
+
+    @Override
+    public boolean isTileOccupied() {
+        return true;
+    }
+}
